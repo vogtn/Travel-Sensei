1196a9a652b2075ddcc46a14f2961d1d7534f76a
vogtn <nicolasjvogt@gmail.com>
2017-03-21 18:40:45 -0700

    still flight api

--- public/app/controllers/CityController.js
+++ public/app/controllers/CityController.js
@@ -5,13 +5,22 @@ angular
   '$state',
   '$stateParams',
   'YelpFactory',
-  function($scope, $state, $stateParams, YelpFactory) {
+  'FlightFactory',
+  function($scope, $state, $stateParams, YelpFactory, FlightFactory) {
     YelpFactory.getCity('nagoya')
     .then(function(res) {
       $scope.topFive = res.data
       console.log('success', res);
     })
     .catch(function(err) {
+      console.log(err);
+    })
+
+    FlightFactory.getCity('nagoya')
+    .then(function(res){
+      $scope.flights = res.data;
+    })
+    .catch(function(err){
       console.log(err);
     })
 
